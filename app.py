4d378e3b9dd05c42eca4f63ad770619d4e115444
Muhu-Muza <schumannivan@gmail.com>
2022-10-26 20:02:42 +0300

    edit categories

--- app.py
+++ app.py
@@ -99,32 +99,28 @@ def create_category(username):
             return redirect(url_for('categories', username = username))
     return render_template('create_category.html', username = username)
 
-# @app.route("/edit_category/<username>/<category_name>", methods = ['GET', 'POST'])
-# @login_is_required
-# def edit_category(category_name):
-#     if request.method =="POST":
-#         new_name = request.form['category']
-#         if new_name:
-#             user = users[session['username']]           
-#             user.add_category(new_name)
-#             user.categories[new_name] = user.categories[category_name]
-#             user.categories[new_name]["new_description"] = user.categories[category_name]["description"]
-#             user.delete_category(category_name)
-#             username = user.username
-#             return redirect(url_for('categories', username = username))
-#     return render_template('edit_category.html', category_name = category_name)
+@app.route("/edit_category/<category_name>", methods = ['GET', 'POST'])
+@login_is_required
+def edit_category(category_name):
+    if request.method =="POST":
+        new_name = request.form['category']
+        new_description = request.form['description']
+        if new_name:
+            user = users[session['username']]           
+            user.add_category(new_name, new_description)
+            user.categories[new_name] = user.categories[category_name]
+            user.categories[new_name]["new_description"] = user.categories[category_name]["description"]
+            user.delete_category(category_name)
+            username = user.username
+            return redirect(url_for('categories', username = username))
+    return render_template('edit_category.html', category_name = category_name)
 
-# @app.route('/update_category/<name>', methods=['GET','POST'])
-# def update_category(name):
-#     if request.method == "POST":
-#         new_name = request.form['catname']
-#         if new_name:
-#             usr = users[session['username']]
-#             usr.create_categories(new_name)
-#             usr.categories[new_name] = usr.categories[name]
-#             usr.delete_category(name)
-#             return redirect(url_for('user',username=usr.username))
-#     return render_template('updatecategory.html', name=name)
+@app.route('/delete_category/<name>')
+@login_is_required
+def delete_category(name):
+    usr = users[session['username']]
+    usr.delete_category(name)
+    return redirect(url_for('user', username=usr.username))
 
 @app.route("/<username>/categories")
 def categories(username):
